7efb7c78827a07ab550d1cfdd6facf21474c69c5
benaryorg <binary@benary.org>
2024-01-25 17:19:37 +0000

    pkgs.formats: negative type checking tests
    Signed-off-by: benaryorg <binary@benary.org>

--- pkgs/pkgs-lib/tests/formats.nix
+++ pkgs/pkgs-lib/tests/formats.nix
@@ -1,24 +1,21 @@
 { pkgs }:
 let
   inherit (pkgs) lib formats;
-in
-with lib;
-let
 
-  evalFormat = format: args: def:
-    let
-      formatSet = format args;
-      config = formatSet.type.merge [] (imap1 (n: def: {
-        # We check the input values, so that
-        #  - we don't write nonsensical tests that will impede progress
-        #  - the test author has a slightly more realistic view of the
-        #    final format during development.
-        value = lib.throwIfNot (formatSet.type.check def) (builtins.trace def "definition does not pass the type's check function") def;
-        file = "def${toString n}";
-      }) [ def ]);
-    in formatSet.generate "test-format-file" config;
+  # merging allows us to add metadata to the input
+  # this makes error messages more readable during development
+  mergeInput = name: format: input:
+    format.type.merge [] [
+      {
+        # explicitly throw here to trigger the code path that prints the error message for users
+        value = lib.throwIfNot (format.type.check input) (builtins.trace input "definition does not pass the type's check function") input;
+        # inject the name
+        file = "format-test-${name}";
+      }
+    ];
 
-  runBuildTest = name: { drv, expected }: pkgs.runCommand name {
+  # run a diff between expected and real output
+  runDiff = name: drv: expected: pkgs.runCommand name {
     passAsFile = ["expected"];
     inherit expected drv;
   } ''
@@ -31,12 +28,48 @@ let
     fi
   '';
 
-  runBuildTests = tests: pkgs.linkFarm "nixpkgs-pkgs-lib-format-tests" (mapAttrsToList (name: value: { inherit name; path = runBuildTest name value; }) (filterAttrs (name: value: value != null) tests));
+  # use this to check for proper serialization
+  shouldPass = format: input: expected: name: {
+    name = "pass-${name}";
+    path = runDiff "test-format-${name}" (format.generate "test-format-${name}" (mergeInput name format input)) expected;
+  };
+
+  # use this function to assert that a type check must fail
+  # note that as per 352e7d330a26 and 352e7d330a26 the type checking of attrsets and lists are not strict
+  # this means that primarily the type check will catch custom module types
+  shouldFail = format: input: name: {
+    name = "pass-${name}";
+    path = if !(format.type.check input) then
+        # the check failing is what we want, so don't do anything here
+        pkgs.runCommand "test-format-${name}" {} "touch $out"
+      else
+        # bail with some verbose information in case the type check passes
+        pkgs.runCommand "test-format-${name}" {
+            passAsFile = [ "inputText" ];
+            testName = name;
+            # toXML seems to be the only way to stringify input that is generic over *any* data type
+            inputText = builtins.toXML input;
+          }
+          ''
+            echo "Type check $testName passed when it shouldn't."
+            echo "The following data was used as input:"
+            echo
+            cat "$inputTextPath"
+            exit 1
+          '';
+  };
+
+  runBuildTests = (lib.flip lib.pipe) [
+    lib.attrsToList
+    (builtins.map ({ name, value }: value name))
+    (pkgs.linkFarm "nixpkgs-pkgs-lib-format-tests")
+  ];
 
 in runBuildTests {
 
-  testJsonAtoms = {
-    drv = evalFormat formats.json {} {
+  jsonAtoms = shouldPass
+    (formats.json {})
+    {
       null = null;
       false = false;
       true = true;
@@ -46,8 +79,8 @@ in runBuildTests {
       attrs.foo = null;
       list = [ null null ];
       path = ./formats.nix;
-    };
-    expected = ''
+    }
+    ''
       {
         "attrs": {
           "foo": null
@@ -65,10 +98,10 @@ in runBuildTests {
         "true": true
       }
     '';
-  };
 
-  testYamlAtoms = {
-    drv = evalFormat formats.yaml {} {
+  yamlAtoms = shouldPass
+    (formats.yaml {})
+    {
       null = null;
       false = false;
       true = true;
@@ -77,8 +110,8 @@ in runBuildTests {
       attrs.foo = null;
       list = [ null null ];
       path = ./formats.nix;
-    };
-    expected = ''
+    }
+    ''
       attrs:
         foo: null
       'false': false
@@ -91,35 +124,43 @@ in runBuildTests {
       str: foo
       'true': true
     '';
-  };
 
-  testIniAtoms = {
-    drv = evalFormat formats.ini {} {
+  iniAtoms = shouldPass
+    (formats.ini {})
+    {
       foo = {
         bool = true;
         int = 10;
         float = 3.141;
         str = "string";
       };
-    };
-    expected = ''
+    }
+    ''
       [foo]
       bool=true
       float=3.141000
       int=10
       str=string
     '';
-  };
 
-  testIniDuplicateKeys = {
-    drv = evalFormat formats.ini { listsAsDuplicateKeys = true; } {
+  iniNoAttrsets = shouldFail
+    (formats.ini {})
+    {
+      foo = {
+        bar = { baz = "quux"; };
+      };
+    };
+
+  iniDuplicateKeys = shouldPass
+    (formats.ini { listsAsDuplicateKeys = true; })
+    {
       foo = {
         bar = [ null true "test" 1.2 10 ];
         baz = false;
         qux = "qux";
       };
-    };
-    expected = ''
+    }
+    ''
       [foo]
       bar=null
       bar=true
@@ -129,46 +170,46 @@ in runBuildTests {
       baz=false
       qux=qux
     '';
-  };
 
-  testIniListToValue = {
-    drv = evalFormat formats.ini { listToValue = concatMapStringsSep ", " (generators.mkValueStringDefault {}); } {
+  iniListToValue = shouldPass
+    (formats.ini { listToValue = lib.concatMapStringsSep ", " (lib.generators.mkValueStringDefault {}); })
+    {
       foo = {
         bar = [ null true "test" 1.2 10 ];
         baz = false;
         qux = "qux";
       };
-    };
-    expected = ''
+    }
+    ''
       [foo]
       bar=null, true, test, 1.200000, 10
       baz=false
       qux=qux
     '';
-  };
 
-  testKeyValueAtoms = {
-    drv = evalFormat formats.keyValue {} {
+  keyValueAtoms = shouldPass
+    (formats.keyValue {})
+    {
       bool = true;
       int = 10;
       float = 3.141;
       str = "string";
-    };
-    expected = ''
+    }
+    ''
       bool=true
       float=3.141000
       int=10
       str=string
     '';
-  };
 
-  testKeyValueDuplicateKeys = {
-    drv = evalFormat formats.keyValue { listsAsDuplicateKeys = true; } {
+  keyValueDuplicateKeys = shouldPass
+    (formats.keyValue { listsAsDuplicateKeys = true; })
+    {
       bar = [ null true "test" 1.2 10 ];
       baz = false;
       qux = "qux";
-    };
-    expected = ''
+    }
+    ''
       bar=null
       bar=true
       bar=test
@@ -177,23 +218,23 @@ in runBuildTests {
       baz=false
       qux=qux
     '';
-  };
 
-  testKeyValueListToValue = {
-    drv = evalFormat formats.keyValue { listToValue = concatMapStringsSep ", " (generators.mkValueStringDefault {}); } {
+  keyValueListToValue = shouldPass
+    (formats.keyValue { listToValue = lib.concatMapStringsSep ", " (lib.generators.mkValueStringDefault {}); })
+    {
       bar = [ null true "test" 1.2 10 ];
       baz = false;
       qux = "qux";
-    };
-    expected = ''
+    }
+    ''
       bar=null, true, test, 1.200000, 10
       baz=false
       qux=qux
     '';
-  };
 
-  testTomlAtoms = {
-    drv = evalFormat formats.toml {} {
+  tomlAtoms = shouldPass
+    (formats.toml {})
+    {
       false = false;
       true = true;
       int = 10;
@@ -202,8 +243,8 @@ in runBuildTests {
       attrs.foo = "foo";
       list = [ 1 2 ];
       level1.level2.level3.level4 = "deep";
-    };
-    expected = ''
+    }
+    ''
       false = false
       float = 3.141
       int = 10
@@ -216,14 +257,14 @@ in runBuildTests {
       [level1.level2.level3]
       level4 = "deep"
     '';
-  };
 
   # This test is responsible for
   #   1. testing type coercions
   #   2. providing a more readable example test
   # Whereas java-properties/default.nix tests the low level escaping, etc.
-  testJavaProperties = {
-    drv = evalFormat formats.javaProperties {} {
+  javaProperties = shouldPass
+    (formats.javaProperties {})
+    {
       floaty = 3.1415;
       tautologies = true;
       contradictions = false;
@@ -236,8 +277,8 @@ in runBuildTests {
       "ütf 8" = "dûh";
       # NB: Some editors (vscode) show this _whole_ line in right-to-left order
       "الجبر" = "أكثر من مجرد أرقام";
-    };
-    expected = ''
+    }
+    ''
       # Generated with Nix
 
       1 = 2
@@ -249,5 +290,4 @@ in runBuildTests {
       \u00fctf\ 8 = d\u00fbh
       \u0627\u0644\u062c\u0628\u0631 = \u0623\u0643\u062b\u0631 \u0645\u0646 \u0645\u062c\u0631\u062f \u0623\u0631\u0642\u0627\u0645
     '';
-  };
 }
